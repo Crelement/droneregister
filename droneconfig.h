a0a40e71b925eb7014b2dbf07faf86d09ae6c661
Crelement <3569802166@qq.com>
2026-04-22 18:04:09 +0800

    check

--- droneconfig.h
+++ droneconfig.h
@@ -12,34 +12,37 @@ struct dronebank{
     float weight;
 };
 
+struct dronebank bufferzone;
+
 struct dronebank *droneArray;   // 声明一个指针，尚未分配空间
 int capacity=10;              // 记录当前分配的最大元素个数
 int count=0;                  //count是总在册人数,number是编号
 float min_weight=0;
 
-int input(int number){
-    printf("请输入姓名\n");
+uint8_t input(int number){
+    printf("please enter your name\n");
     scanf("%s",droneArray[number].name);
-    printf("请输入密码\n");
+    printf("enter your password\n");
     scanf("%s",droneArray[number].password);
-    printf("请输入电话号码\n");
+    printf("enter your phonenumber\n");
     scanf("%s",droneArray[number].phonenum);
-    printf("请输入飞机类型\n");
+    printf("your dronetype\n");
     scanf("%s",droneArray[number].dronetype);
-    printf("请输入飞机重量\n");
-    scanf("%f",droneArray[number].weight);
+    printf("the weight of your drone\n");
+    scanf("%f",&droneArray[number].weight);
     return 1;
 }
 
-void save(int i){
+void save(){
 }
 
-int memorycontrol(int capacity){
+/*int memorycontrol(){
     droneArray=(struct dronebank*)malloc(capacity*sizeof(struct dronebank));
     if(droneArray==NULL) return 0;
-}
+}*/
 
-int memoryexpand(int capacity,int count){
+int memoryexpand(){
+    droneArray=(struct dronebank *)realloc(droneArray,capacity*sizeof(struct dronebank));
     if(count>=capacity){
         capacity+=6;
         droneArray=(struct dronebank*)realloc(droneArray,capacity*sizeof(struct dronebank));
@@ -47,76 +50,89 @@ int memoryexpand(int capacity,int count){
     if(droneArray==NULL){
         return 0;
     }
+    return 1;
 }
 
 float sort(int number,float min_weight){
     if(droneArray[number].weight<=min_weight){
         min_weight=droneArray[number].weight;
-        memcpy(&droneArray[capacity-1].weight,&droneArray[number-1].weight,sizeof(struct dronebank));
+        memcpy(&bufferzone.weight,&droneArray[number-1].weight,sizeof(struct dronebank));
         memcpy(&droneArray[number-1].weight,&droneArray[number].weight,sizeof(struct dronebank));
-        memcpy(&droneArray[number].weight,&droneArray[capacity-1].weight,sizeof(struct dronebank));
+        memcpy(&droneArray[number].weight,&bufferzone.weight,sizeof(struct dronebank));
     }
     return min_weight;
 }
 
-int registe(int command,int count){   //command==2为登录 command==1是注册
-    if(command==1){
-        int number=count;
-        input(number);
-        if(!input(number)){
-            number=count-1;
-            return number;
-        }
-        return number;
-    }
+uint8_t registe(){   //command==2为登录 command==1是注册
+   count++;
+   memoryexpand();
+   input(count-1);
+   min_weight=sort(count-1,min_weight);  
+    return 0;
 }
 
 void display(int number){
-    printf("%s\n",droneArray[number].name);
-    printf("%s\n",droneArray[number].password);
-    printf("%s\n",droneArray[number].phonenum);
-    printf("%s\n",droneArray[number].dronetype);
-    printf("%f\n",droneArray[number].weight);
+    printf("username: %s\n",droneArray[number].name);
+    printf("password: %s\n",droneArray[number].password);
+    printf("phonenumber: %s\n",droneArray[number].phonenum);
+    printf("dronetype: %s\n",droneArray[number].dronetype);
+    printf("weight: %f\n",droneArray[number].weight);
+    if(droneArray[number].weight>=250){
+        printf("need special certificates CAAC\n");
+    }
 }
 
-int login(char *name,char * password,int count){
-    for(int i=0;i<count;i++){
-        if(droneArray[i].name==*name && droneArray[i].password==*password){
-            display(i);
-        }else{
-            printf("error");
-        }
-    }
-    return 0;
-}
+
+
 
 void list(){
     for(int i=0;i<count;++i){
         printf("%s\n",droneArray[i].name);
         printf("%s\n",droneArray[i].dronetype);
     }
+    system("pause");
+
 }
 
-int verify(char *name,char *password){
+int login(){
+    char name[25];
+    char password[20];
+    printf("please enter yourname\n");
+    scanf("%s",name);
+    printf("enter the password\n");
+    scanf("%s",password);
     for(int i=0;i<count;++i){
         if(!strcmp(droneArray[i].name,name)&&!strcmp(droneArray[i].password,password)){
+            display(i);
+            system("pause");
             return 1;
         }
+      
     }
+    printf("error user or password\n");
+      system("pause");
     return 0;
 }
 
+
+
 void menu(){
-    printf("Select the function you want to use\n");
+    printf("Welcome to drone assignment system\n");
     printf("1.Register\n");
     printf("2.Login\n");
     printf("3.List\n");
     printf("4.Exit\n");
     int c=_getch()-'0';
     switch(c){
-        case 1:
-        case 2:registe(c,count);break;
+        case 1:registe();break;
+        case 2:login();break;
         case 3:list();break;
-        case 4:exit(0);
+        case 4:
+        save();
+        printf("presented by Cr\n");
+        system("pause");
+        exit(0);
     }
 }
+
+
--- droneregister.c
+++ droneregister.c
@@ -1,5 +1,12 @@
 #include"droneconfig.h"
-
+uint8_t u;
 int main(){
     FILE *fp=fopen("info.txt","w");
+    
+    while(1){
+     menu();
+     system("cls");
+    }
+       
+    return 0;
 }
